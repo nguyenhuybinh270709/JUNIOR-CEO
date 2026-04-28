de74cab274a8da8caeb1600b83b38f8b64bcd092
Nguyen Huy Binh <nguyenhuybinh270709@gmail.com>
2026-04-28 21:20:56 +0700

    chore: add background color to black instead of depend on device theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       lang="vi"
       className={`${montserrat.className} font-main h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col bg-black">
         <Navbar />
         <main className="min-h-screen">{children}</main>
         <Footer />
